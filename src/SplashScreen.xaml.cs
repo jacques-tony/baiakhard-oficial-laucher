136b651d58d3fcb98d42314515ecc78600b45688
jacques-tony <jakiin.tr@gmail.com>
2023-11-06 14:22:16 -0300

    json update

--- src/SplashScreen.xaml.cs
+++ src/SplashScreen.xaml.cs
@@ -18,7 +18,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class SplashScreen : Window
 	{
-		static string launcerConfigUrl = "https://raw.githubusercontent.com/jacques-tony/phoenix-launcher/main/launcher_config.json";
+		static string launcerConfigUrl = "https://raw.githubusercontent.com/jacques-tony/LaucherPhoenixBeta/main/launcher_config.json";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
