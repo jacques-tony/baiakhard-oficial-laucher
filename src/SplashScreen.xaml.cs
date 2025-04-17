554b4e113adc9f063b23990611f8626f59b52264
jacques-tony <jakiin.tr@gmail.com>
2025-04-17 10:23:32 -0300

    feat: update to hardbaiak info

--- src/SplashScreen.xaml.cs
+++ src/SplashScreen.xaml.cs
@@ -18,7 +18,7 @@ namespace CanaryLauncherUpdate
 {
 	public partial class SplashScreen : Window
 	{
-		static string launcerConfigUrl = "https://raw.githubusercontent.com/jacques-tony/phoenixlaucher/main/launcher_config.json";
+		static string launcerConfigUrl = "https://raw.githubusercontent.com/jacques-tony/baiakhard-oficial-laucher/refs/heads/main/launcher_config.json";
 		// Load informations of launcher_config.json file
 		static ClientConfig clientConfig = ClientConfig.loadFromFile(launcerConfigUrl);
 
